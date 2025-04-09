04a40d0bf3c288853fb6ccf4ed3d73eeaae7b2b1
daniel-ndeto <danielndeto222@gmail.com>
2025-04-09 21:52:31 +0300

    first upload

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+// Importing the logo image
+import logo from "./images/logo.png";
+
+// Importing the banner image used in the application
+import bannerImg from "./images/bannerImg.png";
+
+// Importing images for different projects
+import projectOne from "./images/projects/projectOne.jpg";
+import projectTwo from "./images/projects/projectTwo.jpg";
+import projectThree from "./images/projects/projectThree.jpeg";
+
+// Importing testimonial-related images
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+
+// Importing the contact section image
+import contactImg from "./images/contact/contactImg.png";
+
+// Exporting all imported assets for use in other parts of the application
+export {
+  logo,
+  bannerImg,
+  projectOne,
+  projectTwo,
+  projectThree,
+  testimonialOne,
+  testimonialTwo,
+  quote,
+  contactImg,
+};
---
+++ src/components/banner/Media.jsx
@@ -0,0 +1,50 @@
+// imports of media icons
+import React from 'react'
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+const Media = () => {
+  // This component displays social media icons and best skill icons
+  return (
+    <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaTwitter />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        {/* This section displays the best skills of the developer */}
+         {/* The icons are displayed using react-icons library */}
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            <span className="bannerIcon">
+              <SiFigma />
+            </span>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Media
